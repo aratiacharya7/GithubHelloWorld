b4fa2e0bf625b413bb2e4268b384d44d537b5d50
Arati Acharya <arati.acharya.2023@student.lhschools.org>
2020-02-27 11:12:50 -0500

    called Hello World Method again

--- src/GithubHelloWorld.java
+++ src/GithubHelloWorld.java
@@ -1,15 +1,19 @@
 public class GithubHelloWorld {
     public static void main(String[] args) {
-                System.out.println("Hello World");
-                helloWorld();
+        System.out.println("Hello World");
+        helloWorld();
+        myName();
     }
 
-    public static void helloWorld(){
+    public static void helloWorld() {
 
         System.out.println("Hello Github World");
     }
 
+    public static void myName() {
 
+        System.out.println("This is Arati Acharya");
+    }
 
 
 }
